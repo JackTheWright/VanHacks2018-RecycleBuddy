84d6e8f25ea78ab794f6572eecaf6891acc9cef8
Jack Wright <jackwright@Jacks-MacBook-Pro.local>
2018-09-23 00:15:37 -0700

    FINAL

---
+++ VanHacksJAGM/VanHacksJAGM/RecycleAddVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VanHacksJAGM
+//
+//  Created by Jack Wright on 2018-09-22.
+//  Copyright © 2018 Jagms. All rights reserved.
+//
+
+import UIKit
+
+class RecycleAddVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+
+}
+
