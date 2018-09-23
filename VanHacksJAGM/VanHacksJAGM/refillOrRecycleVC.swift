18a4b1d8c251a9cd4356761384a1e0808ceb2e0e
Jack Wright <jackwright@Jacks-MacBook-Pro.local>
2018-09-22 23:32:08 -0700

    bottle count works or refil or recycle page

---
+++ VanHacksJAGM/VanHacksJAGM/refillOrRecycleVC.swift
@@ -0,0 +1,23 @@
+//
+//  refillOrRecycleVC.swift
+//  VanHacksJAGM
+//
+//  Created by Jack Wright on 2018-09-22.
+//  Copyright © 2018 Jagms. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class RefillOrRecycleVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let scoreTotal = UserDefaults.standard.integer(forKey: "totalScore")
+        bottles.text = String(scoreTotal) + " Bottles"
+        
+    }
+    @IBOutlet weak var bottles: UILabel!
+    
+}
