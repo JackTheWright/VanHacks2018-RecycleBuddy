df3acee3350b5ce856aa8d996a861da37617d002
Alex Brown <alex.98.brown@gmail.com>
2018-09-23 01:21:35 -0700

    Added Last few View Controllers, Did some Layouts, Connected some stuff, yup
    Pretty much the title, but did two pages, one a pop up for after submit button is clicked and also linked to thanks page from submit as well

---
+++ VanHacksJAGM/VanHacksJAGM/MilestonePopController.swift
@@ -0,0 +1,33 @@
+//
+//  MilestonePopController.swift
+//  VanHacksJAGM
+//
+//  Created by Alex Brown on 2018-09-23.
+//  Copyright © 2018 Jagms. All rights reserved.
+//
+
+import UIKit
+
+class MilestonePopController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func Close(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ VanHacksJAGM/VanHacksJAGM/MilestonePopUp.swift
@@ -0,0 +1,13 @@
+//
+//  MilestonePopUp.swift
+//  VanHacksJAGM
+//
+//  Created by Alex Brown on 2018-09-23.
+//  Copyright © 2018 Jagms. All rights reserved.
+//
+
+import UIKit
+
+class MilestonePopUp: NSObject {
+
+}
